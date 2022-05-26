d6c9f9d1b9efcafb1936f66364409ab28ba4bf9f
Gal Levy <gallevy@edu.unifil.br>
2022-05-26 17:44:25 -0300

    Corrigindo encoding/crossover + mutacao

--- Encoding/fjsspReadDict.py
+++ Encoding/fjsspReadDict.py
@@ -8,10 +8,11 @@ from operator import itemgetter
 from job import Job
 
 # Kacem 4 Jobs x 5 Machines
+#file = 'Encoding/datasets/test.fjs'
 file = 'Encoding/datasets/Kacem1_4x5.fjs'
 
 # Kacem 15 Jobs x 10 Machines
-#file = 'datasets/Kacem4.fjs'
+#file = 'Encoding/datasets/Kacem4.fjs'
 
 # A list of Jobs
 jobs_list = []
@@ -62,7 +63,6 @@ with open(file) as data:
         # set next job to be processed
         currentJob += 1
 
-#print(jobs_list[0][1][0][0])
 print("List of ", len(jobs_list), " JOBs created")
 
 for j in range(len(jobs_list)):
@@ -72,15 +72,18 @@ for j in range(len(jobs_list)):
 print("\n")
 
 #encoding
-SolutionNum = 5
+SolutionNum = 100
 SolutionList = []
 for i in range(SolutionNum):
     Solution = []
     for x in range(len(jobs_list)):
         for y in jobs_list[x]:
             #choose option rendomly to append to a solution being made
-            OperationChosen = jobs_list[x][y][random.randint(0, int(float(max_operations))-1)]
-            Solution.append(OperationChosen)
+            OperationChosen = jobs_list[x][y][random.randint(0, len(jobs_list[x][y])-1)]
+            Grade = len(jobs_list[x][y])
+            for key in range(len(jobs_list[x][y])):
+                if OperationChosen[1] > jobs_list[x][y][int(key)-1][1]: Grade = Grade -1
+            Solution.append(Grade)
     SolutionList.append(Solution)
 
 #print list os solutions
@@ -91,17 +94,39 @@ print("\n")
 
 
 #crossover
-s1 = SolutionList[random.randint(0, int(float(SolutionNum))-1)]
-s2 = SolutionList[random.randint(0, int(float(SolutionNum))-1)]
-Half = round(len(s1)/2)
-s1h1 = s1[:Half]
-s1h2 = s1[Half:]
-s2h1 = s2[:Half]
-s2h2 = s2[Half:]
-s2 = s1h1 + s2h2
-s1 = s2h1 + s1h2
+ParsCrossover = 1
+CrossoverList = []
+for i in range(ParsCrossover):
+    #getting two rendom solutions, cutting them in half and mixing them one with another
+    s1 = SolutionList[random.randint(0, int(float(SolutionNum))-1)]
+    s2 = SolutionList[random.randint(0, int(float(SolutionNum))-1)]
+    Half = round(len(s1)/2)
+    s1h1 = s1[:Half]
+    s1h2 = s1[Half:]
+    s2h1 = s2[:Half]
+    s2h2 = s2[Half:]
+    s2 = s1h1 + s2h2
+    s1 = s2h1 + s1h2
+    CrossoverList.append(s2)
+    CrossoverList.append(s1)
 
 #print new solutions made with crossover
 print("New solutions made with crossover:")
-print("Solution 1: ",s1)
-print("Solution 2: ",s2)
+print("Solution 1: ",CrossoverList[0])
+print("Solution 2: ",CrossoverList[1])
+print("\n")
+
+#mutation
+for i in range(len(CrossoverList)):
+    #print("I: ",i)
+    for x in range(len(jobs_list)):
+        for y in jobs_list[x]:
+            #print("len(jobs_list[x][y]) : ", len(jobs_list[x][y])," jobs_list[x][i+1]: ",len(jobs_list[x][i+1]))
+            if len(jobs_list[x][y]) == len(jobs_list[x][i+1]): operatioNum = len(jobs_list[x][y]) 
+    #print(operatioNum)           
+    CrossoverList[i][random.randint(0, len(CrossoverList[i])-1)] = random.randint(1, operatioNum)
+
+#Print new solutions after mutation
+print("New solutions made with Mutation:")
+print("Solution 1: ",CrossoverList[0])
+print("Solution 2: ",CrossoverList[1])
