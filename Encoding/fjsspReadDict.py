767b140bc401c3ec981f7a9966270a8eeb239a94
Gal Levy <gallevy@edu.unifil.br>
2022-06-09 10:11:22 -0300

    GA simples funcionando + streamlit para front

--- Encoding/fjsspReadDict.py
+++ Encoding/fjsspReadDict.py
@@ -73,7 +73,7 @@ for j in range(len(jobs_list)):
 print("\n")
 
 #encoding
-SolutionNum = 100
+SolutionNum = 10
 SolutionList = []
 for i in range(SolutionNum):
     Solution = []
@@ -87,54 +87,7 @@ for i in range(SolutionNum):
             Solution.append(Grade)
     SolutionList.append(Solution)
 
-#print list os solutions
-print("Solution list:")
-for i in range(SolutionNum):
-    print("Solution #",i,": ",SolutionList[i])
-print("\n")
-
-
-#crossover
-ParsCrossover = 1 #bug nao aceita mais de um par!!!
-CrossoverList = []
-for i in range(ParsCrossover):
-    #getting two rendom solutions, cutting them in half and mixing them one with another
-    s1 = SolutionList[random.randint(0, int(float(SolutionNum))-1)]
-    s2 = SolutionList[random.randint(0, int(float(SolutionNum))-1)]
-    Half = round(len(s1)/2)
-    s1h1 = s1[:Half]
-    s1h2 = s1[Half:]
-    s2h1 = s2[:Half]
-    s2h2 = s2[Half:]
-    s2 = s1h1 + s2h2
-    s1 = s2h1 + s1h2
-    CrossoverList.append(s2)
-    CrossoverList.append(s1)
-
-#print new solutions made with crossover
-print("New solutions made with crossover:")
-print("Solution 1: ",CrossoverList[0])
-print("Solution 2: ",CrossoverList[1])
-print("\n")
-
-#mutation
-for i in range(len(CrossoverList)):
-    for x in range(len(jobs_list)):
-        for y in jobs_list[x]:
-            if len(jobs_list[x][y]) == len(jobs_list[x][i+1]): operatioNum = len(jobs_list[x][y])           
-    CrossoverList[i][random.randint(0, len(CrossoverList[i])-1)] = random.randint(1, operatioNum)
-
-#Print new solutions after mutation
-print("New solutions made with Mutation:")
-print("Solution 1: ",CrossoverList[0])
-print("Solution 2: ",CrossoverList[1])
-print("\n")
-
-#makespan Vector for each index on encoding
-#Duas listas, uma com tempo de job e outra com tempo da máquina, toda vez que for pega o tempo do job adiciona tempo da maquina rodando, depois de adicioonar pega
-#esse numero e adiciona na lista para falar o tempoo do job e o proxima operaçao tem que ser iniciado depois desse tempo
-
-#array that stores each machine and time for each operation on each job globally
+#calculating incial makespan
 timeMachineOrganized = []
 for x in range(len(jobs_list)):
     for y in jobs_list[x]:
@@ -144,24 +97,16 @@ for x in range(len(jobs_list)):
             timeMachineScrambled.append(jobs_list[x][y][z])
         timeMachineOrganizedTemp = sorted(timeMachineScrambled, key=lambda x: x[1])
         timeMachineOrganized.append(timeMachineOrganizedTemp)
-solutionMachineTime = []
+solutionMachineTimeI = []
 
-#for each solution index number it search the number to add to solutionMachineTime
-for i in range(len(CrossoverList)):
+#for each solution index number it search the number to add to solutionMachineTimeI
+for i in range(len(SolutionList)):
     timeMachineTemp = []
-    for x in range(len(CrossoverList[i])):
-        timeMachineTemp.append(timeMachineOrganized[x][CrossoverList[i][x] - 1])
-    solutionMachineTime.append(timeMachineTemp)
-
-# Print solution Machines and time after mutation
-print("solution Machines and time after Mutation:")
-print("Solution 1 machines and time: ", solutionMachineTime[0])
-print("Solution 2 machines and time: ", solutionMachineTime[1])
-print("\n")
-
-
-# Caluclating Makespan
-for i in range(len(solutionMachineTime)):
+    for x in range(len(SolutionList[i])):
+        timeMachineTemp.append(timeMachineOrganized[x][SolutionList[i][x] - 1])
+    solutionMachineTimeI.append(timeMachineTemp)
+    
+for i in range(len(solutionMachineTimeI)):
     jobs = []
     machines = []
     jobAtual = 0
@@ -170,17 +115,128 @@ for i in range(len(solutionMachineTime)):
         jobs.append(0)
     for machine in range(number_total_machines):
         machines.append(0)
-    for solution in range(len(solutionMachineTime[i])):     
+    for solution in range(len(solutionMachineTimeI[i])):     
         if(contador > len(jobs_list[jobAtual])-1): 
             jobAtual += 1
             contador = 0
         contador += 1
-        if(machines[solutionMachineTime[i][solution][0]-1] >= jobs[jobAtual]):
-            machines[solutionMachineTime[i][solution][0]-1] += solutionMachineTime[i][solution][1]
-            jobs[jobAtual] = machines[solutionMachineTime[i][solution][0]-1]
+        if(machines[solutionMachineTimeI[i][solution][0]-1] >= jobs[jobAtual]):
+            machines[solutionMachineTimeI[i][solution][0]-1] += solutionMachineTimeI[i][solution][1]
+            jobs[jobAtual] = machines[solutionMachineTimeI[i][solution][0]-1]
         else:
-            jobs[jobAtual] += solutionMachineTime[i][solution][1] 
-            machines[solutionMachineTime[i][solution][0]-1] = jobs[jobAtual]
-    print('Solution ',solutionMachineTime[i],' makespans: ',max(machines))
-    print('\n')
+            jobs[jobAtual] += solutionMachineTimeI[i][solution][1] 
+            machines[solutionMachineTimeI[i][solution][0]-1] = jobs[jobAtual]
+    solutionMachineTimeI[i].append(max(machines))
 
+#print list os solutions
+print("Solution list:")
+for i in range(SolutionNum):
+    print("Solution #",i,": ",SolutionList[i])
+print("\n")
+
+generationNum = 10
+for i in range(generationNum):
+    crossoverChance = 0.8
+    mutationChance = 0.6
+    for x in range(SolutionNum):
+        CrossoverList = []
+        validator = 0
+        if(random.random() <= crossoverChance):
+            #crossover
+            #getting two rendom solutions, cutting them in half and mixing them one with another
+            s1 = SolutionList[random.randint(0, int(float(SolutionNum))-1)]
+            s2 = SolutionList[random.randint(0, int(float(SolutionNum))-1)]
+            Half = round(len(s1)/2)
+            s1h1 = s1[:Half]
+            s1h2 = s1[Half:]
+            s2h1 = s2[:Half]
+            s2h2 = s2[Half:]
+            s2 = s1h1 + s2h2
+            s1 = s2h1 + s1h2
+            CrossoverList.append(s2)
+            CrossoverList.append(s1)
+            validator +=1
+
+            #print new solutions made with crossover
+            print("New solutions made with crossover:")
+            for i in range(len(CrossoverList)):
+                print("Solution ",i,": ",CrossoverList[i])
+            print("\n")
+
+        if(random.random() <= mutationChance and validator == 1):
+            #mutation
+            for i in range(len(CrossoverList)):
+                for x in range(len(jobs_list)):
+                    for y in jobs_list[x]:
+                        operatioNum = len(jobs_list[x][y])       
+                CrossoverList[i][random.randint(0, len(CrossoverList[i])-1)] = random.randint(1, operatioNum)
+
+
+            #Print new solutions after mutation
+            print("New solutions made with Mutation:")
+            for i in range(len(CrossoverList)):
+                print("Solution ",i,": ",CrossoverList[i])
+            print("\n")
+
+        #makespan Vector for each index on encoding
+        #Duas listas, uma com tempo de job e outra com tempo da máquina, toda vez que for pega o tempo do job adiciona tempo da maquina rodando, depois de adicioonar pega
+        #esse numero e adiciona na lista para falar o tempoo do job e o proxima operaçao tem que ser iniciado depois desse tempo
+
+        #array that stores each machine and time for each operation on each job globally
+        if(validator == 1):
+            timeMachineOrganized = []
+            for x in range(len(jobs_list)):
+                for y in jobs_list[x]:
+                    timeMachineScrambled = []
+                    # Clone the vector into another one so it can organize
+                    for z in range(len(jobs_list[x][y])):
+                        timeMachineScrambled.append(jobs_list[x][y][z])
+                    timeMachineOrganizedTemp = sorted(timeMachineScrambled, key=lambda x: x[1])
+                    timeMachineOrganized.append(timeMachineOrganizedTemp)
+            solutionMachineTime = []
+
+            #for each solution index number it search the number to add to solutionMachineTime
+            for i in range(len(CrossoverList)):
+                timeMachineTemp = []
+                for x in range(len(CrossoverList[i])):
+                    timeMachineTemp.append(timeMachineOrganized[x][CrossoverList[i][x] - 1])
+                solutionMachineTime.append(timeMachineTemp)
+
+            # Print solution Machines and time after mutation
+            print("solution Machines and time after Mutation:")
+            for i in range(len(solutionMachineTime)):
+                print("Solution ",i,": ",solutionMachineTime[i])
+            print("\n")
+
+
+            # Caluclating Makespan
+            for i in range(len(solutionMachineTime)):
+                jobs = []
+                machines = []
+                jobAtual = 0
+                contador = 0
+                for job in range(number_total_jobs):
+                    jobs.append(0)
+                for machine in range(number_total_machines):
+                    machines.append(0)
+                for solution in range(len(solutionMachineTime[i])):     
+                    if(contador > len(jobs_list[jobAtual])-1): 
+                        jobAtual += 1
+                        contador = 0
+                    contador += 1
+                    if(machines[solutionMachineTime[i][solution][0]-1] >= jobs[jobAtual]):
+                        machines[solutionMachineTime[i][solution][0]-1] += solutionMachineTime[i][solution][1]
+                        jobs[jobAtual] = machines[solutionMachineTime[i][solution][0]-1]
+                    else:
+                        jobs[jobAtual] += solutionMachineTime[i][solution][1] 
+                        machines[solutionMachineTime[i][solution][0]-1] = jobs[jobAtual]
+                solutionMachineTime[i].append(max(machines))
+                solutionMachineTimeI.append(solutionMachineTime[i])
+
+
+print("Final solution list")
+for i in range(len(solutionMachineTimeI)):
+    print("Solution ",i,": ",solutionMachineTimeI[i])
+print("\n")
+print("\n")
+print(min(solutionMachineTimeI, key=lambda x: x[-1]))
